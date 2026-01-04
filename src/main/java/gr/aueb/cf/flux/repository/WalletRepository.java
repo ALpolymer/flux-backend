353b841e6cc09b0cc9f86c1f3f5c3b55322b18fb
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-04 20:46:52 +0200

    feat: add wallet repository

---
+++ src/main/java/gr/aueb/cf/flux/repository/WalletRepository.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.flux.repository;
+
+import gr.aueb.cf.flux.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long>
+, JpaSpecificationExecutor<Wallet> {
+    Optional<Wallet> findByUuid(String uuid);
+    List<Wallet> findByUser(Long userId);
+}
