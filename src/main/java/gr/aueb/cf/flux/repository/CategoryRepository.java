49bb95ce1163b3de6dc03b365d038c3786ca10dd
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-04 20:52:04 +0200

    feat: add category repository

--- src/main/java/gr/aueb/cf/flux/repository/CategoryRepository.java
+++ src/main/java/gr/aueb/cf/flux/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 package gr.aueb.cf.flux.repository;
 
-import gr.aueb.cf.flux.model.Wallet;
+import gr.aueb.cf.flux.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -9,8 +9,8 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface WalletRepository extends JpaRepository<Wallet, Long>
-, JpaSpecificationExecutor<Wallet> {
-    Optional<Wallet> findByUuid(String uuid);
-    List<Wallet> findByUserId(Long userId);
+public interface CategoryRepository extends JpaRepository<Category, Long>
+, JpaSpecificationExecutor<Category> {
+    Optional<Category> findByUuid(String uuid);
+    List<Category> findByUserId(Long userId);
 }
