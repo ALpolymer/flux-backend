9f542ee65b75dcb3754d8eecb628b497bd12db42
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-11 19:22:00 +0200

    feat(auth): add authenticate endpoint to AuthController

--- src/main/java/gr/aueb/cf/flux/api/AuthController.java
+++ src/main/java/gr/aueb/cf/flux/api/AuthController.java
@@ -1,9 +1,13 @@
 package gr.aueb.cf.flux.api;
 
+import gr.aueb.cf.flux.authentication.AuthenticationService;
 import gr.aueb.cf.flux.core.exceptions.AppObjectAlreadyExists;
+import gr.aueb.cf.flux.dto.AuthRequestDTO;
+import gr.aueb.cf.flux.dto.AuthResponseDTO;
 import gr.aueb.cf.flux.dto.UserInsertDTO;
 import gr.aueb.cf.flux.dto.UserReadOnlyDTO;
 import gr.aueb.cf.flux.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserReadOnlyDTO> register(@RequestBody UserInsertDTO dto) throws AppObjectAlreadyExists {
+    public ResponseEntity<UserReadOnlyDTO> register(@Valid @RequestBody UserInsertDTO dto) throws AppObjectAlreadyExists {
         UserReadOnlyDTO savedUser = userService.saveUser(dto);
 
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponseDTO> authenticate(@RequestBody AuthRequestDTO requestDTO){
+        AuthResponseDTO responseDTO = authenticationService.authenticate(requestDTO);
+        return  new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 }
