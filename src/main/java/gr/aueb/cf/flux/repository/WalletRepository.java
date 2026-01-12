d92cb5381dbd9a1d67e804f9a204da575a9e676c
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-12 12:42:17 +0200

    fix(repository): fix findByUserId parameter

--- src/main/java/gr/aueb/cf/flux/repository/WalletRepository.java
+++ src/main/java/gr/aueb/cf/flux/repository/WalletRepository.java
@@ -13,5 +13,5 @@ import java.util.Optional;
 public interface WalletRepository extends JpaRepository<Wallet, Long>
 , JpaSpecificationExecutor<Wallet> {
     Optional<Wallet> findByUuid(String uuid);
-    List<Wallet> findByUserId(Long walletId);
+    List<Wallet> findByUserId(Long userId);
 }
