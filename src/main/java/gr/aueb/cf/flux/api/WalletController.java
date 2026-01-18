6548c77398efa3129593c0ead99c367e7bfdd829
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-18 16:10:24 +0200

    fix(controller): delete redundant imports

--- src/main/java/gr/aueb/cf/flux/api/WalletController.java
+++ src/main/java/gr/aueb/cf/flux/api/WalletController.java
@@ -9,14 +9,13 @@ import gr.aueb.cf.flux.model.User;
 import gr.aueb.cf.flux.service.IWalletService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Slf4j
+
 @RestController
 @RequestMapping("/api/wallets")
 @RequiredArgsConstructor
