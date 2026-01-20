2034d92af38c53bdc4e19925c6b5b8dff9ba657a
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-20 21:26:18 +0200

    fix(repository): fix typo

--- src/main/java/gr/aueb/cf/flux/repository/CategoryRepository.java
+++ src/main/java/gr/aueb/cf/flux/repository/CategoryRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 public interface CategoryRepository extends JpaRepository<Category, Long>
 , JpaSpecificationExecutor<Category> {
     Optional<Category> findByUuid(String uuid);
-    Optional<Category> findByUuidAndUserId(String uuid, Long UserId);
+    Optional<Category> findByUuidAndUserId(String uuid, Long userId);
     List<Category> findByUserId(Long userId);
 }
