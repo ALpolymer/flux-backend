fabf06feb5fc5dc065680ba68c07f172ed1c4721
Alexandros Polymeros <alpolymer@gmail.com>
2026-01-24 16:08:14 +0200

    feat(openApi): add OpenApiConfig

---
+++ src/main/java/gr/aueb/cf/flux/core/OpenApiConfig.java
@@ -0,0 +1,45 @@
+package gr.aueb.cf.flux.core;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Flux Expense Tracker",
+                description = "An expense tracker API built with SpringBoot ",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Alexandros Polymeros",
+                        email = "alpolymer@gmail.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+
+        ),
+        servers = @Server(
+                description = "LOCAL_ENV",
+                url = "http://localhost:8080"
+        ),
+        security = @SecurityRequirement(
+                name = "bearerAuth"
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
